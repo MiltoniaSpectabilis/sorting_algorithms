85c55d8a16094143f685238fcb31cc2df861ef5b
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-02-21 00:44:42 +0100

    1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: pointer to the pointer of the first node of the list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *prev, *next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		prev = current->prev;
+		next = current->next;
+		while (prev != NULL && current->n < prev->n)
+		{
+			prev->next = next;
+			if (next != NULL)
+				next->prev = prev;
+			current->prev = prev->prev;
+			current->next = prev;
+			if (prev->prev != NULL)
+				prev->prev->next = current;
+			else
+				*list = current;
+			prev->prev = current;
+			print_list(*list);
+			prev = current->prev;
+			next = current->next;
+		}
+		current = next;
+	}
+}
+
